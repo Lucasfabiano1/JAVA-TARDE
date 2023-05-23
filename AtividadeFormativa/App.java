808f160ef9da1a64e80469771acf1c15e15a2b90
Lucasfabiano1 <123977243+Lucasfabiano1@users.noreply.github.com>
2023-05-23 18:54:15 +0000

    23/05

---
+++ AtividadeFormativa/App.java
@@ -0,0 +1,10 @@
+package AtividadeFormativa;
+
+public class App {
+    public static void main(String[] args) {
+        Exercicios obj = new Exercicios();
+      // obj.atvd1();
+       // obj.atvd2();
+        obj.atvd3();
+     }
+}
--- Matriz/App.java
+++ Matriz/App.java
@@ -1,8 +1,8 @@
 public class App {
     public static void main(String[] args) {
         MatrizExemplo obj1 = new MatrizExemplo();
-        //obj1.exemplo1();//
+        obj1.exemplo1();//
         // obj1.exercicio2();
-        obj1.exercicio3();
+        // obj1.exercicio3();
     }
 }
